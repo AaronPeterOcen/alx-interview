dea7baf0fb20d54b262327359008eed6d753cc75
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-25 12:35:26 +0300

    task update on pascal-triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,14 +8,15 @@ def pascal_triangle(n):
     """creating the triangle"""
 
     res = []
-    if n > 0:
-        for i in range(1, n + 1):
-            level = []
+    if n <= 0:
+        return res
+    for i in range(n):
+        tempList = []
 
-            a = 1
-            for j in range(1, i + 1):
-                level.append(a)
-                a = a * (i - j)
-            res.append(level)
-
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                tempList.append(1)
+            else:
+                tempList.append(res[i - 1][j - 1] + res[i - 1][j])
+        res.append(tempList)
     return res
