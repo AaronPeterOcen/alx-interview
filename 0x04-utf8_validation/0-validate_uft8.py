0cdb70531de38a896d3af59247f9faef6577e476
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-05-22 09:43:45 +0300

    updating

--- 0x04-utf8_validation/0-validate_uft8.py
+++ 0x04-utf8_validation/0-validate_uft8.py
@@ -4,29 +4,24 @@
 """
 
 def validUTF8(data):
-  """
-  This function checks if a given data set represents a valid UTF-8 encoding.
-  """
-  def is_start_byte(byte):
+    def is_start_byte(byte):
         return byte >> 6 != 0b10
 
-        num_bytes = 0
+    num_bytes = 0
 
-        for byte in data:
-            if num_bytes == 0:
-                # Determine the number of bytes to follow based on the leading bits of the start byte
-                if byte >> 5 == 0b110:
-                    num_bytes = 1
-                elif byte >> 4 == 0b1110:
-                    num_bytes = 2
-                elif byte >> 3 == 0b11110:
-                    num_bytes = 3
-                elif byte >> 7:
-                    return False
-            else:
-                # If it's a continuation byte, it should start with 10
-                if not (byte >> 6 == 0b10):
-                    return False
-                num_bytes -= 1
+    for byte in data:
+        if num_bytes == 0:
+            if byte >> 5 == 0b110:
+                num_bytes = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes = 3
+            elif byte >> 7:
+                return False
+        else:
+            if not (byte >> 6 == 0b10):
+                return False
+            num_bytes -= 1
 
-        return num_bytes == 0
+    return num_bytes == 0
