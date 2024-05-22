64fe4a71ba38c9d4826aa91522b2cdd77a5ed38f
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-05-22 09:46:54 +0300

    utf valida

--- 0x04-utf8_validation/0-validate_uft8.py
+++ 0x04-utf8_validation/0-validate_uft8.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
 """
- Method that determines if a given data set represents a valid UTF-8 encoding.
+Determines if a given data set represents a valid UTF-8 encoding.
 """
 
+
 def validUTF8(data):
-    def is_start_byte(byte):
-        return byte >> 6 != 0b10
+    """
+    Return: True if data is a valid UTF-8 encoding, else return False.
+    """
+    n_bytes = 0
+    for num in data:
+        bin_rep = format(num, '#010b')[-8:]
+        if n_bytes == 0:
+            for bit in bin_rep:
+                if bit == '0':
+                    break
+                n_bytes += 1
 
-    num_bytes = 0
+            if n_bytes == 0:
+                continue
 
-    for byte in data:
-        if num_bytes == 0:
-            if byte >> 5 == 0b110:
-                num_bytes = 1
-            elif byte >> 4 == 0b1110:
-                num_bytes = 2
-            elif byte >> 3 == 0b11110:
-                num_bytes = 3
-            elif byte >> 7:
+            if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
-            if not (byte >> 6 == 0b10):
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
                 return False
-            num_bytes -= 1
 
-    return num_bytes == 0
+        n_bytes -= 1
+
+    return n_bytes == 0
