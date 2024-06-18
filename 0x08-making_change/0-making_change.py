3b6d9900640eede1df455c47f8df7ef2879d3dd9
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-06-18 10:54:46 +0300

    update making change file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,4 +5,16 @@ Given a pile of coins of different values, determine the fewest number of coins 
 
 
 def makeChange(coins, total):
+    if total == 0:
+        return 0
+    elif total < 0:
+        return -1
 
+    dp = [float("inf")] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float("inf") else -1
