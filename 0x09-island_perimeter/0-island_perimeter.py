e22a1de2f57375a81b2d501eafc570da45f62aea
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-06-27 08:28:08 +0300

    update on island parameters

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""
+Island Perimeter:
+    returns the perimeter of the island described in grid
+"""
+
 
 def island_perimeter(grid):
     """
@@ -12,14 +17,14 @@ def island_perimeter(grid):
     Cells are connected horizontally/vertically (not diagonally).
     grid is rectangular, with its width and height not exceeding 100`
     """
-    rows, cols = len(grid), len(grid[0])
-    perimeter = 4 * sum(sum(row) for row in grid)
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
 
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                perimeter -= 2 * (
-                    (row > 0 and grid[row - 1][col] == 1)
-                    + (col > 0 and grid[row][col - 1] == 1)
-                )
     return perimeter
